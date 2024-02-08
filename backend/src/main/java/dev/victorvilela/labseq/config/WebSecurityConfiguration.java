b85ba01279f78cef655f0e880c734e0b9ca686ea
Victor Soares Vilela <50461887+victorsvilela@users.noreply.github.com>
2024-02-08 17:49:22 +0000

    config for frontend cors

--- backend/src/main/java/dev/victorvilela/labseq/config/WebSecurityConfiguration.java
+++ backend/src/main/java/dev/victorvilela/labseq/config/WebSecurityConfiguration.java
@@ -1,2 +1,14 @@
-package dev.victorvilela.labseq.config;public class dsds {
+package dev.victorvilela.labseq.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebSecurityConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH");
+    }
 }
